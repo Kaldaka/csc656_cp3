c41d0f5985d9ba6f181c981a92feea85e7ffc9d2
Kaldaka <54258907+Kaldaka@users.noreply.github.com>
2023-11-14 17:59:54 -0800

    Update dgemv-openmp.cpp

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -3,7 +3,9 @@
 #include <stdio.h>
 #include <omp.h>
 
+
 const char* dgemv_desc = "OpenMP dgemv.";
+
 
 /*
  * This routine performs a dgemv operation
@@ -12,19 +14,31 @@ const char* dgemv_desc = "OpenMP dgemv.";
  * On exit, A and X maintain their input values.
  */
 
+
 void my_dgemv(int n, double* A, double* x, double* y) {
 
-   #pragma omp parallel
+   /*#pragma omp parallel
    {
       int nthreads = omp_get_num_threads();
       int thread_id = omp_get_thread_num();
       printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
       printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
+   }*/
 
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
-
+   {
+     #pragma omp parallel for
+     for(int row = 0; row < n; row++){
+             int offset = row * n;
+             double sum = 0.0;
+             for(int column = 0; column < n; column++){
+                     int cell = offset + column;
+                     sum += A[cell] * x[column];
+             }
+             #pragma omp atomic
+             y[row] += sum;
+     }
+   }
 }
-
